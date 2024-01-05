ac8dbfe24118ab2f4f52d7befd6a71bdc313870e
prasanna-ezhilmurugan <prasanna.ezhilmurugan@gmail.com>
2024-01-05 15:14:18 +0530

    add neodev plugin

---
+++ lua/plugins/neodev.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/neodev.nvim",
+  opts = {},
+  config = function()
+    require("neodev").setup({
+      library = {
+        plugins = { "nvim-dap-ui" },
+        types = true,
+      }
+    })
+  end,
+}
