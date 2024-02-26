fc9b528c508c7ae8508e5789415caff8f2731619
prasanna-ezhilmurugan <prasanna.ezhilmurugan@gmail.com>
2024-02-26 12:18:43 +0530

    add comment and sessions

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,14 +1,15 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = {
-    'nvim-tree/nvim-web-devicons', 
+    "nvim-tree/nvim-web-devicons",
   },
   config = function()
     local gruvbox_light = require("lualine.themes.gruvbox_light")
     require("lualine").setup({
       options = {
-        theme = gruvbox_light
-      }
+        theme = gruvbox_light,
+      },
+      sections = { lualine_c = { require("auto-session.lib").current_session_name } },
     })
-  end
+  end,
 }
